20f6128d38e1d50255291f042c81dd86cd1ff629
[Rimidun] <1k2>
2021-01-21 15:51:46 +0300

    3 version

--- src/main/java/webapp/MusicPlayer.java
+++ src/main/java/webapp/MusicPlayer.java
@@ -1,6 +1,8 @@
 package webapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -9,19 +11,40 @@ import java.util.List;
 @Component
 public class MusicPlayer {
 
+    @Value("${musicPlayer.name}")
+    private String name;
+    @Value("${musicPlayer.volume}")
+    private int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    private Music music;
+
+    @Autowired
+    @Qualifier("rockMusic")
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
     private ClassicalMusic classicalMusic;
     private RockMusic rockMusic;
 
 
-    @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
-    }
+//    @Autowired
+//    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+//        this.classicalMusic = classicalMusic;
+//        this.rockMusic = rockMusic;
+//    }
 
     public void playMusic() {
 
-        System.out.println("Playing: " + classicalMusic.getSong());
-        System.out.println("Playing: " + rockMusic.getSong());
+        System.out.println("Playing: " + music.getSong());
+        //System.out.println("Playing: " + rockMusic.getSong());
     }
 }
